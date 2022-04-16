048f47abf4d0ea47c8da72d4eb12ee69b46f1529
IteeU <93969956+IteeU@users.noreply.github.com>
2022-04-16 07:37:52 -0700

    Update security-groups.tf

--- terafrom_scripts/security-groups.tf
+++ terafrom_scripts/security-groups.tf
@@ -1,17 +1,22 @@
-resource "aws_security_group" "kubernetes_sg" {
+resource "aws_security_group" "test_sg" {
   name        = "Allow_All_Ports"
   description = "Allow All Ports All Protocals"
-  vpc_id      = aws_vpc.kubernetes.id
+  vpc_id      = aws_vpc.test.id
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
+  
+}
+resource "aws_security_group" "test_sg1" {
+  name        = "Allow_QUAKE_III_ARENA_Ports"
+  description = "Allow Quake III Arena Port"
+  vpc_id      = aws_vpc.test.id
+  ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["173.69.38.20/27950"]
   }
-}
