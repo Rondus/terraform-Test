383412fb9d622bb1ff07e4c1dba1483d5c7bef73
IteeU <93969956+IteeU@users.noreply.github.com>
2022-04-16 07:39:10 -0700

    Update vpc.tf

--- terafrom_scripts/vpc.tf
+++ terafrom_scripts/vpc.tf
@@ -1,44 +1,44 @@
 provider "aws" {
-  region = "us-west-2a"
+  region = "us-west-2"
 }
-resource "aws_vpc" "kubernetes" {
+resource "aws_vpc" "test" {
   cidr_block = var.vpc_cidr
   tags = {
-    Name = "kubernetes"
+    Name = "test"
   }
 }
-resource "aws_internet_gateway" "kubernetes_vpc_igw" {
-  vpc_id = aws_vpc.kubernetes.id
+resource "aws_internet_gateway" "test_vpc_igw" {
+  vpc_id = aws_vpc.test.id
   tags = {
-    Name = "kubernetes_vpc_igw"
+    Name = "test_vpc_igw"
   }
 }
 
-resource "aws_subnet" "kubernetes_subnets" {
+resource "aws_subnet" "test_subnets" {
   count                   = length(var.subnets_cidr)
-  vpc_id                  = aws_vpc.kubernetes.id
+  vpc_id                  = aws_vpc.test.id
   cidr_block              = element(var.subnets_cidr, count.index)
   availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true
   tags = {
-    Name = "kubernetes_subnets_${count.index + 1}"
+    Name = "test_subnets_${count.index + 1}"
   }
 }
 
-resource "aws_route_table" "kubernetes_public_rt" {
-  vpc_id = aws_vpc.kubernetes.id
+resource "aws_route_table" "test_public_rt" {
+  vpc_id = aws_vpc.test.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.kubernetes_vpc_igw.id
+    gateway_id = aws_internet_gateway.test_vpc_igw.id
   }
   tags = {
-    Name = "kubernetes_vpc_public_rt"
+    Name = "test_vpc_public_rt"
   }
 }
 
 
 resource "aws_route_table_association" "rt_sub_association" {
   count          = length(var.subnets_cidr)
-  subnet_id      = element(aws_subnet.kubernetes_subnets.*.id, count.index)
-  route_table_id = aws_route_table.kubernetes_public_rt.id
+  subnet_id      = element(aws_subnet.test_subnets.*.id, count.index)
+  route_table_id = aws_route_table.test_public_rt.id
 }
